08a3e302bacb0abe17d8fffa96384b8a5aa0b5f8
Mollie Peters <mjpete11@asu.edu>
2019-08-15 18:44:16 -0700

    replaced hardcoding

--- Differential_Expression/EdgeR/Sex_and_Tissue/Gene/Normalized/GLM_F_Test/Stringtie/Age_Matched.R
+++ Differential_Expression/EdgeR/Sex_and_Tissue/Gene/Normalized/GLM_F_Test/Stringtie/Age_Matched.R
@@ -154,58 +154,44 @@ Results_df <- lapply(Exact_Res, Summary_Func)
 #---------------------------------------------------------------------------------------------------------------------
 # Test for DGX using quasi-liklihood.
 #---------------------------------------------------------------------------------------------------------------------
-# Make contrasts: Sex by Tissue
-my.contrasts <- makeContrasts(Am.F.vs.M = Amygdala.Female - Amygdala.Male,
-                              At.F.vs.M = Anterior.Female - Anterior.Male,
-                              Ca.F.vs.M = Caudate.Female - Caudate.Male,
-                              Ce.F.vs.M = Cerebellar.Female - Cerebellar.Male,
-                              Co.F.vs.M = Cortex.Female - Cortex.Male,
-                              Fc.F.vs.M = Frontal_Cortex.Female - Frontal_Cortex.Male,
-                              Cm.F.vs.M = Cerebellum.Female - Cerebellum.Male,
-                              Hp.F.vs.M = Hippocampus.Female - Hippocampus.Male,
-                              Hy.F.vs.M = Hypothalamus.Female - Hypothalamus.Male,
-                              Na.F.vs.M = Nucleus_Accumbens.Female - Nucleus_Accumbens.Male,
-                              Pu.F.vs.M = Putamen.Female - Putamen.Male,
-                              Sp.F.vs.M = Spinal_Cord.Female - Spinal_Cord.Male,
-                              Sn.F.vs.M = Substantia_Nigra.Female - Substantia_Nigra.Male,
-                              levels=Design)
+# Fit glm model
+Fit_Func <- function(a, b){
+  fit <- glmQLFit(a, b, robust=TRUE)
+  return(fit)
+}
+Fit <- Map(Fit_Func, a=y, b=Design)
 
-# Make contrasts: Sex by Tissues
-Names <- c('Am.F.vs.M, At.F.vs.M', 'Ca.F.vs.M', 'Ce.F.vs.M, Co.F.vs.M', 'Fc.F.vs.M', 'Cm.F.vs.M', 
+# Make contrasts: Sex by Tissue
+Contrasts <- c('Amygdala.Female - Amygdala.Male',
+               'Anterior.Female - Anterior.Male',
+               'Caudate.Female - Caudate.Male',
+               'Cerebellar.Female - Cerebellar.Male',
+               'Cerebellum.Female - Cerebellum.Male',
+               'Cortex.Female - Cortex.Male',
+               'Frontal_Cortex.Female - Frontal_Cortex.Male',
+               'Hippocampus.Female - Hippocampus.Male',
+               'Hypothalamus.Female - Hypothalamus.Male',
+               'Nucleus_Accumbens.Female - Nucleus_Accumbens.Male',
+               'Putamen.Female - Putamen.Male',
+               'Spinal_Cord.Female - Spinal_Cord.Male',
+               'Substantia_Nigra.Female - Substantia_Nigra.Male')
+
+Names <- c('Am.F.vs.M', 'At.F.vs.M', 'Ca.F.vs.M', 'Ce.F.vs.M', 'Co.F.vs.M', 'Fc.F.vs.M', 'Cm.F.vs.M', 
            'Hp.F.vs.M', 'Hy.F.vs.M', 'Na.F.vs.M', 'Pu.F.vs.M', 'Sp.F.vs.M', 'Sn.F.vs.M')
 
-Contrasts <- c('Amygdala.Female - Amygdala.Male', 'Anterior.Female - Anterior.Male',
-                'Caudate.Female - Caudate.Male', 'Cerebellar.Female - Cerebellar.Male',
-                'Cortex.Female - Cortex.Male', 'Frontal_Cortex.Female - Frontal_Cortex.Male',
-                'Cerebellum.Female - Cerebellum.Male', 'Hippocampus.Female - Hippocampus.Male',
-                'Hypothalamus.Female - Hypothalamus.Male', 'Nucleus_Accumbens.Female - Nucleus_Accumbens.Male',
-                'Putamen.Female - Putamen.Male', 'Spinal_Cord.Female - Spinal_Cord.Male',
-                'Substantia_Nigra.Female - Substantia_Nigra.Male')
-
-Contrast_Func <- function(a, b, c){
-  my.contrasts <- makeContrasts(a = b, levels = c)
-  return(my.contrasts)
+# Make contrasts
+Contrast_Func <- function(a, b){
+  res <- makeContrasts(contrasts = a, levels = colnames(b))
+  return(res)
 }
-GLM_Res <- Map(Contrast_Func, a=Names, b=Contrasts, c=Design)
+my.contrasts <- Map(Contrast_Func, a=Contrasts, b=Design)
+names(my.contrasts) <- Names
 
-# Fit glm model
-fit <- glmQLFit(y, design, robust=TRUE)
-head(fit$coefficients)
-plotQLDisp(fit, main="Quasi-Likelihood Disperson")
-
-# Amygdala Female vs Male F Test
-qlf.Am.F.vs.M <- glmQLFTest(fit, contrast=my.contrasts[,"Am.F.vs.M"])
-df_Am <- summary(decideTests(qlf.Am.F.vs.M))
-grid.newpage() # To keep summary table from plotting on top of other plots
-grid.table(df_Am)
-plotMD(qlf.Am.F.vs.M)
-
-
-# Fit GLM model
-GLM_Func <- function(x, des){
-  glmQLFit(x, contrast=des)
+# Apply GLM F test
+GLM_Ratio_Func <- function(a, b){
+  glmQLFTest(a, contrast=b)
 }
-GLM_Res <- Map(GLM_Func, x=y, des=my.contrasts)
+GLM_Res <- Map(GLM_Ratio_Func, a=Fit, b=my.contrasts)
 
 # Get summary of results
 Summary_Func <- function(x){
@@ -214,10 +200,8 @@ Summary_Func <- function(x){
 }
 Results_df <- lapply(GLM_Res, Summary_Func)
 
-
-
 #---------------------------------------------------------------------------------------------------------------------
-# Example Plots
+# Plots
 #---------------------------------------------------------------------------------------------------------------------
 Titles <- list('Amygdala', 'Anterior', 'Caudate', 'Cerbellum', 'Cerebellar', 'Cortex', 'Frontal Cortex',
                'Hippocampus', 'Hypothalamus', 'Nucleus Accumbens', 'Putamen', 'Spinal Cord', 'Substantia Nigra')
@@ -229,11 +213,11 @@ opar <- par(no.readonly = TRUE)
 par(mfrow = c(3, 5), cex=0.4, mar = c(3, 3, 3, 2), oma =c(6, 6, 6, 2), xpd=TRUE) # margins: c(bottom, left, top, right)
 MD_Plot_Func <- function(x, w){
   plotMD(x, main=w, legend=FALSE, hl.col=c("green", "blue"), cex=1.4)
-  mtext('Mean-Difference Plots', side = 3, outer = TRUE, cex=1.2, line=3)
+  mtext('Hisat: Mean-Difference Plots; GLM F Test', side = 3, outer = TRUE, cex=1.2, line=3)
   mtext('Average log CPM', side = 1, outer = TRUE, line=1)
   mtext('Log-fold-change', side = 2, outer = TRUE, line=2)
 }
-Res_Plots <- Map(MD_Plot_Func, x=Exact_Res, w=Titles)
+Res_Plots <- Map(MD_Plot_Func, x=GLM_Res, w=Titles)
 legend(26.0, 10.0, legend=c("Up","Not Sig", "Down"), pch = 16, col = c("green","black", "blue"), bty = "o", xpd=NA, cex=2.0)
 #par(opar) 
 
@@ -241,7 +225,7 @@ legend(26.0, 10.0, legend=c("Up","Not Sig", "Down"), pch = 16, col = c("green","
 Volcano_Func <- function(x){
   cbind(x$table$logFC, -log10(x$table[,"PValue"]))
 }
-Volcano_Res <- lapply(Exact_Res, Volcano_Func)
+Volcano_Res <- lapply(GLM_Res, Volcano_Func)
 
 # Coerce to df
 Volcano_Res <- lapply(Volcano_Res, as.data.frame)
@@ -274,7 +258,7 @@ Plot_Func <- function(a, b, c){
   with(inner_join(a, c[[2]]), points(logFC, negLogPval, pch=19, col="blue"))
   abline(a=-log10(0.05), b=0, col="blue") 
   abline(v=0, col="red")
-  mtext('Hisat: Volcano Plots', side = 3, outer = TRUE,  cex=1.2, line=3)
+  mtext('Hisat: Volcano Plots; GLM F Test', side = 3, outer = TRUE,  cex=1.2, line=3)
   mtext('logFC', side = 1, outer = TRUE,  cex=0.8, line=1)
   mtext('negLogPval', side = 2, outer = TRUE, line=2)
 }
@@ -282,20 +266,3 @@ Map(Plot_Func, a=Volcano_Res, b=Titles, c=Subset_Res)
 legend(16.0, 9.0, inset=0, legend=c("Positive Significant", "Negative Significant", "Not significant"), 
        pch=16, cex=2.0, col=c("green", "blue", "black"), xpd=NA)
 
-#---------------------------------------------------------------------------------------------------------------------
-# Plots
-#---------------------------------------------------------------------------------------------------------------------
-
-# Amygdala Female vs Male F Test
-qlf.Am.F.vs.M <- glmQLFTest(fit, contrast=my.contrasts[,"Am.F.vs.M"])
-df_Am <- summary(decideTests(qlf.Am.F.vs.M))
-grid.newpage() # To keep summary table from plotting on top of other plots
-grid.table(df_Am)
-plotMD(qlf.Am.F.vs.M)
-
-# Volcano plot
-volcanoData <- cbind(qlf.Am.F.vs.M$table$logFC, -log10(qlf.Am.F.vs.M$table[,"PValue"]))
-colnames(volcanoData) <- c("logFC", "negLogPval")
-plot(volcanoData, pch=19, main='Volcano plot: 1*Amygdala.Female-1*Amygdala.Male')
-abline(a=1.30102999566, b=0, col="blue") # Set intercept equal to p = -log10(0.05)
-abline(v=0, col="red")
