2890f76e944cb38bc52e95c48613800315c42d49
Mollie Peters <mjpete11@asu.edu>
2019-10-20 12:49:27 -0700

    added transcripts

--- Data_Exploration/Multidimensional_Scaling/Salmon/MDS_Tissue.R
+++ Data_Exploration/Multidimensional_Scaling/Salmon/MDS_Tissue.R
@@ -3,12 +3,13 @@
 setwd("/scratch/mjpete11/GTEx/Data_Exploration/Multidimensional_Scaling/Salmon")
 
 METADATA = file.path('/scratch/mjpete11/GTEx/Metadata/', 'Matched_Metadata.csv')
-COUNTS = file.path('/scratch/mjpete11/GTEx/Data_Exploration/Count_Matrices/', 'Count_Matrix.tsv')
-TABLE_DIR =  '/scratch/mjpete11/GTEx/Data_Exploration/Sex_Tissue_Age/Multidimensional_Scaling/Salmon/Matched_Table'
-MDS_K2 = '/scratch/mjpete11/GTEx/Data_Exploration/Multidimensional_Scaling/Salmon/Matched_Plots/Matched_Salmon_MDS_k2.pdf'
-MDS_K4 = '/scratch/mjpete11/GTEx/Data_Exploration/Multidimensional_Scaling/Salmon/Matched_Plots/Matched_Salmon_MDS_k4.pdf'
-REPS_K2 = '/scratch/mjpete11/GTEx/Data_Exploration/Multidimensional_Scaling/Salmon/Matched_Plots/Matched_Reps_Salmon_MDS_k2.pdf'
-REPS_K4 = '/scratch/mjpete11/GTEx/Data_Exploration/Multidimensional_Scaling/Salmon/Matched_Plots/Matched_Reps_Salmon_MDS_k4.pdf'
+GENE_COUNTS = file.path('/scratch/mjpete11/GTEx/Data_Exploration/Count_Matrices/Salmon/', 'Gene_Salmon_CountMatrix.tsv')
+TRANSCRIPT_COUNTS =  file.path('/scratch/mjpete11/GTEx/Data_Exploration/Count_Matrices/Salmon/', 'Transcript_Salmon_CountMatrix.tsv')
+TABLE_DIR =  '/scratch/mjpete11/GTEx/Data_Exploration/Sex_Tissue_Age/Multidimensional_Scaling/Salmon/Matched/Transcript/Matched_Table'
+MDS_K2 = '/scratch/mjpete11/GTEx/Data_Exploration/Multidimensional_Scaling/Salmon/Matched/Transcript/Salmon_MDS_k2.pdf'
+MDS_K4 = '/scratch/mjpete11/GTEx/Data_Exploration/Multidimensional_Scaling/Salmon/Matched/Transcript/Salmon_MDS_k4.pdf'
+REPS_K2 = '/scratch/mjpete11/GTEx/Data_Exploration/Multidimensional_Scaling/Salmon/Matched/Transcript/Reps_Salmon_MDS_k2.pdf'
+REPS_K4 = '/scratch/mjpete11/GTEx/Data_Exploration/Multidimensional_Scaling/Salmon/Matched/Transcript/Reps_Salmon_MDS_k4.pdf'
 
 # Load packages                                                                 
 library(readr)
@@ -24,7 +25,7 @@ Samples = read.csv(METADATA, header = TRUE)
 rownames(Samples) <- Samples$Sample                                             
 
 # Read in count matrix
-cts <- read.table(COUNTS, sep="\t")
+cts <- read.table(TRANSCRIPT_COUNTS, sep="\t")
 
 # Replace . to - in colnames
 colnames(cts) <- str_replace_all(colnames(cts),pattern = "\\.","-")
@@ -56,6 +57,7 @@ Check_Func <- function(a, b){
   Check <- all(rownames(a) %in% colnames(b))
 }
 Res_1 <- Map(Check_Func, a=Meta, b=Tissue_Count)
+all(Res_1==TRUE)
 
 # Match order of samples in metadata and count data
 Match_Order <- function(a, b){ # Will throw an error if dfs are different lengths
@@ -68,6 +70,7 @@ Match_Check <- function(a, b){
   Match <- all(rownames(a) == colnames(b))
 }
 Res_2 <- Map(Match_Check, a=Meta, b=Tissue_Count)
+all(Res_2==TRUE)
 
 # Create DGEList object for each tissue count matrix
 DGE_lst <- lapply(Tissue_Count, function(x){
@@ -90,14 +93,14 @@ MDS_FUN_k2 <- function(DGE, NAME, META, TOP) {
                     dim.plot = c(1,2), 
                     col = colors[META[['Sex']]],
                     main = NAME)
-  mtext('Salmon MDS Plots: Dimensions 1 and 2; Top 100 Most Variable Genes', side=3, outer=TRUE, line=3)
+  mtext('Salmon MDS Plots: Dimensions 1 and 2; Top 100 Most Variable Transcripts', side=3, outer=TRUE, line=3)
   mtext('Dimension 1', side = 1, outer = TRUE, line=1)
   mtext('Dimension 2', side = 2, outer = TRUE, line=2)
   return(k2_MDS)
 }
 pdf(MDS_K2)
 par(mfrow = c(4, 3), cex=0.4, mar = c(3, 2, 2, 2), oma =c(5, 5, 6, 2), xpd=TRUE) # margins: c(bottom, left, top, right)
-k2_MDS <- Map(MDS_FUN_k2, DGE = DGE_Without_Reps, NAME = names(DGE_Without_Reps), META = Meta_Without_Reps, TOP = 100, DIM=c(1,2))
+k2_MDS <- Map(MDS_FUN_k2, DGE = DGE_Without_Reps, NAME = names(DGE_Without_Reps), META = Meta_Without_Reps, TOP = 100)
 legend(5.0, 2.5, inset=0, legend=levels(Meta$Amygdala$Sex), pch=16, cex=2.0, col=colors, xpd=NA)
 dev.off()
 
@@ -105,9 +108,9 @@ dev.off()
 DGE_Reps <- DGE_lst[c(4:7)]
 Meta_Reps <- Meta[c(4:7)]
 
-pdf(MDS_K2)
-par(mfrow = c(4, 3), cex=0.4, mar = c(3, 2, 2, 2), oma =c(5, 5, 6, 2), xpd=TRUE) # margins: c(bottom, left, top, right)
-k2_MDS <- Map(MDS_FUN_k2, DGE = DGE_Reps, NAME = names(DGE_Reps), META = Meta_Reps, TOP = 100)
+pdf(REPS_K2)
+par(mfrow = c(2, 2), cex=0.4, mar = c(3, 2, 2, 6), oma =c(5, 5, 6, 6), xpd=TRUE)
+k2_MDS_Reps <- Map(MDS_FUN_k2, DGE = DGE_Reps, NAME = names(DGE_Reps), META = Meta_Reps, TOP = 100)
 legend(5.0, 2.5, inset=0, legend=levels(Meta$Amygdala$Sex), pch=16, cex=2.0, col=colors, xpd=NA)
 dev.off()
 
@@ -121,7 +124,7 @@ MDS_FUN_k4 <- function(DGE, NAME, META, TOP) {
                     dim.plot = c(3,4), 
                     col = colors[META$Sex],
                     main = NAME)
-  mtext('Salmon MDS Plots: Dimensions 3 and 4; Top 100 Most Variable Genes', side=3, outer=TRUE, line=3)
+  mtext('Salmon MDS Plots: Dimensions 3 and 4; Top 100 Most Variable Transcripts', side=3, outer=TRUE, line=3)
   mtext('Dimension 3', side = 1, outer = TRUE, line=1)
   mtext('Dimension 4', side = 2, outer = TRUE, line=2)
   return(k2_MDS)
@@ -135,7 +138,7 @@ dev.off()
 # Replicates only
 pdf(REPS_K4)
 par(mfrow = c(2, 2), cex=0.4, mar = c(3, 2, 2, 6), oma =c(5, 5, 6, 6), xpd=TRUE)
-k4_MDS <- Map(MDS_FUN_k4, DGE = DGE_Reps, NAME = names(DGE_Reps), META = Meta_Reps, TOP = 100)
+k4_MDS_Reps <- Map(MDS_FUN_k4, DGE = DGE_Reps, NAME = names(DGE_Reps), META = Meta_Reps, TOP = 100)
 legend(1.5, 2.5, inset=0, legend=levels(Meta$Cortex$Sex), pch=16, cex=2.0, col=colors, xpd=NA)
 dev.off()
 
@@ -143,5 +146,8 @@ dev.off()
 setwd(TABLE_DIR)
 sapply(names(k2_MDS), 
        function (x) write.table(k2_MDS[[x]]['cmdscale.out'], file=paste(x, "txt", sep=".")))
+sapply(names(k2_MDS_Reps), 
+       function (x) write.table(k2_MDS[[x]]['cmdscale.out'], file=paste(x, "txt", sep=".")))
+
 
 
