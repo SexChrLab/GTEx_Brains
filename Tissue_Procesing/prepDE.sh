075fcc18c94238bf471006794a07c83878880713
Mollie Peters <mjpete11@asu.edu>
2019-12-16 14:10:06 -0700

    annotated gene & trans count matrices by tissue

--- Tissue_Procesing/prepDE.sh
+++ Tissue_Procesing/prepDE.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-#SBATCH -N 1
-#SBATCH -n 28
-#SBATCH -t 2:00:00 
-#SBATCH --job-name=prepDE
-#SBATCH -o slurm.%j.out
-#SBATCH -e slurm.%j.err
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=mjpete11@asu.edu
+ #SBATCH -N 1
+ #SBATCH -n 28
+ #SBATCH -t 00:02:00
+ #SBATCH --job-name=prepDE
+ #SBATCH -o slurm.%j.out
+ #SBATCH -e slurm.%j.err
+ #SBATCH --mail-type=END,FAIL
+ #SBATCH --mail-user=mjpete11@asu.edu
 
-python2.7 prepDE.py -i /scratch/mjpete11/GTEx/Tissue_Procesing/Anterior/Anterior_Files.txt
-python2.7 prepDE.py -i /scratch/mjpete11/GTEx/Tissue_Procesing/Caudate/Caudate_Files.txt
-python2.7 prepDE.py -i /scratch/mjpete11/GTEx/Tissue_Procesing/Cerebellar/Cerebellar_Files.txt
-python2.7 prepDE.py -i /scratch/mjpete11/GTEx/Tissue_Procesing/Cerebellum/Cerebellum_Files.txt
-python2.7 prepDE.py -i /scratch/mjpete11/GTEx/Tissue_Procesing/Cortex/Cortex_Files.txt
-python2.7 prepDE.py -i /scratch/mjpete11/GTEx/Tissue_Procesing/Frontal_Cortex/Frontal_Cortex_Files.txt
-python2.7 prepDE.py -i /scratch/mjpete11/GTEx/Tissue_Procesing/Hippocampus/Hippocampus_Files.txt
-python2.7 prepDE.py -i /scratch/mjpete11/GTEx/Tissue_Procesing/Hypothalamus/Hypothalamus_Files.txt
-python2.7 prepDE.py -i /scratch/mjpete11/GTEx/Tissue_Procesing/Nucleus_Accumbens/Nucleus_Accumbens_Files.txt
-python2.7 prepDE.py -i /scratch/mjpete11/GTEx/Tissue_Procesing/Putamen/Putamen_Files.txt
-python2.7 prepDE.py -i /scratch/mjpete11/GTEx/Tissue_Procesing/Spinal_Cord/Spinal_Cord_Files.txt
-python2.7 prepDE.py -i /scratch/mjpete11/GTEx/Tissue_Procesing/Substantia_Nigra/Substantia_Nigra_Files.txt
+# iterate through dirs and apply prepDE.py to file in dir begining with dir name
+for i in $(ls /scratch/mjpete11/GTEx/Tissue_Procesing/) # this will try to iteerate through all objects in dir
+do
+    dir=$(readlink -f $i) # iterate through symbolic dir links
+    file=${i%/*} # match until backslash
+    python2.7 prepDE.py -i $dir/"$file"_Files.txt -g $dir/"$file"_gene_matrix.csv -t $dir/"$file"_transcript_matrix.csv # gene and transcript output matrix paths
+done
