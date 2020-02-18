a24137675b908093a9e96d154a439b59814eca9c
Mollie Peters <mjpete11@asu.edu>
2020-02-18 13:59:13 -0700

    refactored test result filtering and volcano plots

--- Differential_Expression/EdgeR/Sex_and_Tissue/Exact_Test/Salmon/Gene_AgeMatched_Exact.R
+++ Differential_Expression/EdgeR/Sex_and_Tissue/Exact_Test/Salmon/Gene_AgeMatched_Exact.R
@@ -3,14 +3,22 @@
 setwd("/scratch/mjpete11/GTEx/Differential_Expression/EdgeR/Sex_and_Tissue/Exact_Test/Salmon")
 
 # Input
-METADATA <- snakemake@input[[1]]
-COUNTS <- snakemake@input[[2]] 
+#METADATA <- snakemake@input[[1]]
+#COUNTS <- snakemake@input[[2]] 
+#
+## Output
+#MD_PLOT <- snakemake@output[[1]]
+#VOLCANO_PLOT <- snakemake@output[[2]]
+#UP_JSON <- snakemake@output[[3]]
+#DOWN_JSON <-snakemake@output[[4]]
+#TABLE <- snakemake@output[[5]]
 
-# Output
-MD_PLOT <- snakemake@output[[1]]
-VOLCANO_PLOT <- snakemake@output[[2]]
-UP_JSON <- snakemake@output[[3]]
-DOWN_JSON <-snakemake@output[[4]]
+METADATA <- '/scratch/mjpete11/GTEx/Metadata/Age_Matched_Metadata.csv'
+COUNTS <- file.path('/scratch/mjpete11/GTEx/Count_Matrices/Salmon/', 'Gene_Salmon_CountMatrix.tsv')
+MD_PLOT <- '/scratch/mjpete11/GTEx/Differential_Expression/EdgeR/Sex_and_Tissue/Exact_Test/Salmon/Age_Matched/Gene/Salmon_Exact_MD.pdf'
+VOLCANO_PLOT <- '/scratch/mjpete11/GTEx/Differential_Expression/EdgeR/Sex_and_Tissue/Exact_Test/Salmon/Age_Matched/Gene/Salmon_Exact_Volcano.pdf'
+UP_JSON <- '/scratch/mjpete11/GTEx/Differential_Expression/EdgeR/Sex_and_Tissue/Exact_Test/Salmon/Age_Matched/Gene/Salmon_Upreg_Exact.json'
+DOWN_JSON <- '/scratch/mjpete11/GTEx/Differential_Expression/EdgeR/Sex_and_Tissue/Exact_Test/Salmon/Age_Matched/Gene/Salmon_Downreg_Exact.json'
 
 # Load packages                                                                 
 library(tximport)                                                               
@@ -66,6 +74,7 @@ check <- list()
 for (i in 1:13){
   check[[i]] <- colnames(res[[i]]) %in% subset(Samples$Sample, Samples$Tissue==Tissues[[i]])
 }
+all(as.logical(lapply(check, all)))
 
 # Create design matrix: Step 1
 # Create sex and tissue factor
@@ -162,18 +171,26 @@ Exact_Res <- Map(Exact_Func, y, Pairs)
 #---------------------------------------------------------------------------------------------------------------------
 # Summarize results 
 #---------------------------------------------------------------------------------------------------------------------
-# Report sig DGX genes
+# Function to correct for multiple testing
+Test_Correct <- function(x){
+  x[['table']][['PValue']] <- p.adjust(x[['table']][['PValue']],method="BH")
+  return(x)
+}
+Exact_Res <- lapply(Exact_Res, Test_Correct)
+
+# Function to filter p-vals, and filter by logFC
 Up_Reg <- function(x){
-  res <- topTags(x, n=Inf, p=0.05)$table
-  up <- res[res$logFC > 0, ]
-  return(up)
+  res <- x[['table']][x[['table']][['PValue']] < 0.05, ] 
+  res <- res[res[['logFC']] > 0, ]
+  return(res)
 }
 
 Down_Reg <- function(x){
-  res <- topTags(x, n=Inf, p=0.05)$table
-  down <- res[res$logFC < 0, ]
-  return(down)
+  res <- x[['table']][x[['table']][['PValue']] < 0.05, ] 
+  res <- res[res[['logFC']] < 0, ]
+  return(res)
 }
+
 Up_Top <- lapply(Exact_Res, Up_Reg)
 Down_Top <- lapply(Exact_Res, Down_Reg)
 
@@ -198,13 +215,13 @@ Summary_Func <- function(x){
   return(res)
 }
 Results_df <- lapply(Exact_Res, Summary_Func)
-
+write(Results_df, TABLE)
 #---------------------------------------------------------------------------------------------------------------------
 # Mean-Difference Plots
 #---------------------------------------------------------------------------------------------------------------------
 # Plot Mean-Difference  plots on one page
 MD_Plot_Func <- function(x, w){
-  plotMD(x, main=w, legend=FALSE, hl.col=c("green", "blue"), cex=1.4)
+  plotMD(x, main=w, p.value=0.05, adjust.method="BH", legend=FALSE, hl.col=c("green", "blue"), cex=1.4)
   mtext('Salmon: Gene Mean-Difference Plots; Exact Test', side = 3, outer = TRUE, cex=1.2, line=3)
   mtext('Average log CPM', side = 1, outer = TRUE, line=1)
   mtext('Log-fold-change', side = 2, outer = TRUE, line=2)
@@ -214,19 +231,28 @@ MD_Plot_Func <- function(x, w){
 pdf(MD_PLOT)
 par(mfrow = c(3, 5), cex=0.4, mar = c(3, 3, 3, 2), oma =c(6, 6, 6, 2), xpd=TRUE)  # margins: c(bottom, left, top, right)
 Res_Plots <- Map(MD_Plot_Func, x=Exact_Res, w=Tissues)
-legend(50.0, 15.0, legend=c("Up","Not Sig", "Down"), pch = 16, col = c("green","black", "blue"), bty = "o", xpd=NA, cex=2.0)
+legend(20.0,0.0, legend=c("Up","Not Sig", "Down"), pch = 16, col = c("green","black", "blue"), bty = "o", xpd=NA, cex=2.0)
 dev.off()
 
 #---------------------------------------------------------------------------------------------------------------------
 # Volcano Plots
 #---------------------------------------------------------------------------------------------------------------------
-# Make df of values for axis
+# Make df of neg log p-vals and logFC results after correcting for multiple testing 
 Volcano_Func <- function(x){
-  cbind(x$table$logFC, -log10(x$table[,"PValue"]))
+  cbind(x[["logFC"]], -log10(x[["PValue"]]))
 }
-Volcano_Res <- lapply(Exact_Res, Volcano_Func)
+Volcano_Up <- lapply(Up_Top, Volcano_Func)
+Volcano_Down <- lapply(Down_Top, Volcano_Func) 
 
-# Coerce to df
+# Function to make df of log p-vals and logFC on untransformed exact test results
+Untrans_Volcano <- Func <- function(x){
+      cbind(x$table$logFC, -log10(x$table[,"PValue"]))
+}
+Volcano_Res <- lapply(Exact_Res, Untrans_Volcano)
+
+# Coerce to df from mtx
+Volcano_Up <- lapply(Volcano_Up, as.data.frame)
+Volcano_Down <- lapply(Volcano_Down, as.data.frame)
 Volcano_Res <- lapply(Volcano_Res, as.data.frame)
 
 # Rename columns
@@ -235,13 +261,24 @@ colnames <- c("logFC", "negLogPval")
 Rename_Cols_Func <- function(x){
   setNames(x, colnames)
 }
+Volcano_Up <- lapply(Volcano_Up, Rename_Cols_Func)
+Volcano_Down <- lapply(Volcano_Down, Rename_Cols_Func)
 Volcano_Res <- lapply(Volcano_Res, Rename_Cols_Func)
 
+head(Volcano_Res[[1]])
+head(Volcano_Up[[1]])
+
+# Set ylim and xlim
+xmax <- ceiling(max(as.numeric(lapply(Volcano_Res, function(x) max(x[['logFC']])))))
+xmin <- ceiling(min(as.numeric(lapply(Volcano_Res, function(x) min(x[['logFC']])))))
+ymax <- ceiling(max(as.numeric(lapply(Volcano_Res, function(x) max(x[['negLogPval']])))))
+ymin <- ceiling(min(as.numeric(lapply(Volcano_Res, function(x) min(x[['negLogPval']])))))
+
 # Plot
-Plot_Func <- function(a, b, c, d){
-  plot(a, pch=19, main=b, xlab = '', ylab = '', las = 1)
-  with(inner_join(a, c), points(logFC, negLogPval, pch=19, col="green"))
-  with(inner_join(a, d), points(logFC, negLogPval, pch=19, col="blue"))
+Plot_Func <- function(RES, TISSUE, UP, DOWN){
+  plot(RES, pch=19, main=TISSUE, xlab = '', ylab = '', las = 1, ylim=c(ymin, ymax), xlim=c(xmin,xmax))
+  with(UP, points(logFC, negLogPval, pch=19, col="green"))
+  with(DOWN, points(logFC, negLogPval, pch=19, col="blue"))
   abline(a=-log10(0.05), b=0, col="blue") 
   abline(v=c(2,-2), col="red")
   mtext('Salmon: Gene Volcano Plots; Exact Test', side = 3, outer = TRUE,  cex=1.2, line=3)
@@ -250,7 +287,7 @@ Plot_Func <- function(a, b, c, d){
 }
 pdf(VOLCANO_PLOT)
 par(mfrow = c(3, 5), cex=0.4, mar = c(2, 2, 4, 2), oma =c(6, 6, 6, 2), xpd=FALSE)
-Map(Plot_Func, a=Volcano_Res, b=Tissues, c=Up_Top, d=Down_Top)
+Map(Plot_Func, RES=Volcano_Res, TISSUE=Tissues, UP=Volcano_Up, DOWN=Volcano_Down)
 legend(25.0, 8.0, inset=0, legend=c("Positive Significant", "Negative Significant", "Not significant"), 
        pch=16, cex=2.0, col=c("green", "blue", "black"), xpd=NA)
 dev.off()
