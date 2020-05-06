0eefc59f250d33768ca3298646e7465c96b5b873
Mollie Peters <mjpete11@asu.edu>
2020-05-05 17:24:28 -0700

    prep df for mashr

--- Data_Exploration/Mashr/Mashr.R
+++ Data_Exploration/Mashr/Mashr.R
@@ -6,18 +6,18 @@ library(data.table)
 # Cols: gene ID followed by tissue type, Rows: gene, Values: logFC
 # Exact/Salmon/Age/Gene, Exact/Age/Salmon/Trans, Exact/Salmon/Match/Gene, Exact/Salmon/Match/Trans
 # Paths to csv files
+# Salmon, Exact
 p1 <- "/scratch/mjpete11/GTEx/Differential_Expression/Exact_Test/Salmon/Age_Matched/Gene"
-# Read in csv files as list of dfs (they are actually tab delim...)
-ESAG <- list.files(path = p1, pattern = "*.csv", full.names=T)
-ESAG
-res <- Map(fread, header=T, ESAG)
 
-# Name each df in list
-names(res) <- c("Amygdala", "Anterior", "Caudate", "Cerebellar", "Cerebellum", "Cortex",
-                "Frontal_Cortex", "Hippocampus", "Hypothalamus", "Nucleus_Accumbens", 
-                "Putamen", "Spinal_Cord", "Substantia_Nigra")
+# Read in csv files as list of dfs (they are actually tab delim...);,pass list of file names with list.files
+EASG.lst <- Map(fread, header=FALSE, list.files(path = p1, pattern = "*.csv", full.names=T))
 
-# Name columns then drop last two by name
+# Assign names to dfs in list
+names(EASG.lst) <- c("Amygdala", "Anterior", "Caudate", "Cerebellar", "Cerebellum", "Cortex",
+                 "Frontal_Cortex", "Hippocampus", "Hypothalamus", "Nucleus_Accumbens", 
+                 "Putamen", "Spinal_Cord", "Substantia_Nigra")
+
+# Name items in list, name the df columns then drop last two cols by name
 Name_Drop <- function(x){
     x <- data.frame(x)
     colnames(x) <- c("gene_ID", "logFC", "logCPM", "PValue")
@@ -25,15 +25,37 @@ Name_Drop <- function(x){
     x <- x[,!colnames(x) %in% drp]
     return(x)
 }    
-tmp <- lapply(res, Name_Drop)
+EASG.lst<- lapply(EASG.lst, Name_Drop)
 
+# 
 # Left join list of dfs by gene_ID col; if not present in a df, fill with zero
-test <- Reduce(function(df1, df2) merge(df1, df2, by="gene_ID", all=TRUE), tmp)
-colnames(test)[2:ncol(test)] <- names(res)
+EASG.df <- Reduce(function(df1, df2) merge(df1, df2, by="gene_ID", all=TRUE), EASG.lst)
+rownames(EASG.df) <- EASG.df[["gene_ID"]]
+EASG.df[["gene_ID"]] <- NULL
+colnames(EASG.df)[1:ncol(EASG.df)] <- names(EASG.lst)
+EASG.df[is.na(EASG.df)] <- 0
 
+# Check for missing or non-finite values
+any(is.nan(EASG.df$logFC)) # No NANs
+all(is.finite(EASG.df$logFC)) # All values are finite
 
+# Add an standard error matrix with ncol, nrow equal to the effect matrix; 
+# populate with 1s since the se is the same for all samples
+#EASG.se <- data.frame(matrix(1, nrow=nrow(EASG.df), ncol=ncol(EASG.df))) 
+#rownames(EASG.se) <- rownames(EASG.df) 
+#colnames(EASG.se) <- colnames(EASG.df) 
+#EASG.lst <- list(Bhat=EASG.df, Shat=EASG.se)
+
+# Create the mashr object using the pre-made standard error matrix
+#mash.EASG <- mash_set_data(EASG.lst$Bhat, EASG.lst$Shat)
+
+# Create mashr object using option to automatically generate the standard error matrix (fill with 1s)
+mash.EASG <- mash_set_data(Bhat=EASG.df, Shat=1)
 
 # Create the mashr data object
+# Simulate data
+set.seed(1)
+simdata = simple_sims(500,5,1)
 mash.age = mash_set_data(simdata$Bhat, simdata$Shat)
 
 # Compute canonical covariance matrices
