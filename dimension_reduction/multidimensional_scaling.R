c40e4a68dbe95a836b35950f2b52896b81022cac
Mollie Peters <mjpete11@asu.edu>
2020-06-22 18:25:36 -0700

    subset counts with metadata

--- dimension_reduction/multidimensional_scaling.R
+++ dimension_reduction/multidimensional_scaling.R
@@ -11,62 +11,77 @@ MDS <- file.path(BASE, "dimension_reduction/MDS_plots/MDS.pdf")
 
 # Load packages
 library(data.table)
-library(readr)
+#library(readr)
 library(stringr)
-library(dplyr)
-library(limma)
+#library(dplyr)
+#library(limma)
 library(edgeR)
 
 # Read in files                                                            
-Meta <- read.table(METADATA, header = TRUE, sep=",")
-Counts <- as.data.frame(fread(COUNTS)) 
+meta <- read.table(METADATA, header = TRUE, sep=",", stringsAsFactors=FALSE)
+counts <- data.frame(fread(COUNTS))
 
-# Quick view
-head(Meta)
-head(colnames(Counts))
+# Drop gene name and ID from counts df
+counts <- counts[,3:ncol(counts)]
+
+# Replace . to - in colnames
+colnames(counts) <- str_replace_all(colnames(counts),pattern = "\\.","-")
+
+# Drop samples in metadata that do not have count data
+select_samples<-colnames(counts)[colnames(counts)%in%meta$Sample_ID]
+meta <- meta[meta$Sample_ID %in% select_samples,]
+
+# Subset counts to only samples present in metadata
+counts <- counts[,select_samples]
+
+# Remove genes with cpm < 1 in each sex
+group <- factor(meta$Sex)
+counts <- DGEList(counts, group=group)
+keep <- filterByExpr(counts, design=group, min.count=1, min.prop=0.5)
+counts <- counts[keep, ,keep.lib.sizes=FALSE]
+
+# limma-voom normalization
+counts <- voom(counts, design=group)
 
 # Set rownames of metadata object equal to sample names
-rownames(Meta) <- Meta$Sample_ID
+rownames(meta) <- meta$Sample_ID
 
 # Make list of lists of samples for each tissue
 Tissue_Lst <- list()
-for(i in 1:length(levels(Meta$Tissue))){
-		  Tissue_Lst[[i]] <- as.vector(Meta[Meta$Tissue == levels(Meta$Tissue)[i], "Sample_ID"])
+for(i in 1:length(levels(meta$Tissue))){
+		  Tissue_Lst[[i]] <- as.vector(meta[meta$Tissue == levels(meta$Tissue)[i], "Sample_ID"])
 }
 
 # Rename lists in list as tissue names
-names(Tissue_Lst) <- levels(Meta$Tissue)
+names(Tissue_Lst) <- levels(meta$Tissue)
 
 # Split counts into list of dfs by tissue
 Tissue_Count <- list()
 for(i in 1:length(Tissue_Lst)){
-		  Tissue_Count[[i]] <- Counts[,which(colnames(Counts) %in% Tissue_Lst[[i]])]
+		  Tissue_Count[[i]] <- counts[,which(colnames(counts) %in% Tissue_Lst[[i]])]
 }
-names(Tissue_Count) <- levels(Meta$Tissue)
+names(Tissue_Count) <- levels(meta$Tissue)
 
-# Metadata split into list of dfs by tissue
-Meta_Lst <- list()
-for(i in 1:length(levels(Meta$Tissue))){
-		  Meta_Lst[[i]] <- Meta[Meta$Tissue == levels(Meta$Tissue)[i],]
+# metadata split into list of dfs by tissue
+meta_Lst <- list()
+for(i in 1:length(levels(meta$Tissue))){
+		  meta_Lst[[i]] <- meta[meta$Tissue == levels(meta$Tissue)[i],]
 }
-names(Meta_Lst) <- levels(Meta$Tissue)
+names(meta_Lst) <- levels(meta$Tissue)
 
 # Check that rownames equals colnames 
 Check <- function(a, b){
 	all(rownames(a) %in% colnames(b))
 }
-Res_1 <- Map(Check, a=Meta, b=Tissue_Count)
+Res_1 <- Map(Check, a=meta, b=Tissue_Count)
 all(Res_1==TRUE)
 
 # Check that order matches
 Match_Check <- function(a, b){
 	 Match <- all(rownames(a) == colnames(b))
 }
-Res_2 <- Map(Match_Check, a=Meta, b=Tissue_Count)
+Res_2 <- Map(Match_Check, a=meta, b=Tissue_Count)
 all(Res_2==TRUE)
-
-# Create DGEList object for each tissue count matrix
-#DGE_Lst <- lapply(Tissue_Count, function(x){ DGEList(x)}) 
 
 # MDS plots on one page
 colors <- c("blue", "darkgreen")
@@ -91,9 +106,9 @@ par(mfrow = c(4, 3), cex=0.4, mar = c(3, 2, 2, 2), oma =c(5, 5, 6, 2), xpd=TRUE)
 Map(MDS_Plot, 
 	DGE = Tissue_Count, 
 	NAME = names(Tissue_Count), 
-	META = Meta_Lst, 
+	META = meta_Lst, 
 	TITLE='GTEx v8 MDS Plots: Dimensions 1 and 2; Top 100 Most Variable Genes')
 # female will be blue because color in plot/legend assigned alphabetically
-legend(5.0, 2.5, inset=0, legend=levels(Meta$Sex), pch=16, cex=2.0, col=colors, xpd=NA)
+legend(10.0, 2.5, inset=0, legend=levels(meta$Sex), pch=16, cex=2.0, col=colors, xpd=NA)
 dev.off()
 
