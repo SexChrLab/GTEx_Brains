0f2f95102b48e8c8a443d0bd807b46d940030b2f
Mollie Peters <mjpete11@asu.edu>
2020-07-14 12:17:46 -0700

    dropped sex chr from umap

--- dimension_reduction/umap.R
+++ dimension_reduction/umap.R
@@ -5,7 +5,6 @@
 # Load libraries
 library(umap)
 library(ggplot2)
-library(RColorBrewer)
 library(data.table)
 library(stringr)
 library(edgeR)
@@ -16,10 +15,33 @@ BASE <- "/scratch/mjpete11/human_monkey_brain"
 # Input
 METADATA <- file.path(BASE, "data/metadata/metadata.csv")
 COUNTS <- file.path(BASE, "data/counts/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct")
+CHRX <- file.path(BASE, "data/gene_annotation/gencodeGenes_Xchr.txt")
+CHRY <- file.path(BASE, "data/gene_annotation/gencodeGenes_Ychr.txt")
+
+# Output
+PROJECTION <- file.path(BASE, "dimension_redcution/umap_plots/umap_NoSexChr.txt")
 
 # Read in files
+xchr <- read.table(CHRX, sep = "")
+ychr <- read.table(CHRY, sep = "")
 meta <- read.table(METADATA, header = TRUE, sep = ",", stringsAsFactors = FALSE)
 gene_counts <- data.frame(fread(COUNTS))
+
+#_______________________________________________________________________________
+# Drop X and Y-linked genes to check if clustering is driven by sex-linked loci
+#_______________________________________________________________________________
+# Original number of genes
+nrow(gene_counts) # 56,200
+
+# Remove X and Y-linked genes
+rows_to_drop <- intersect(xchr$V6, gene_counts$Name)
+rows_to_drop <- c(rows_to_drop, intersect(ychr$V6, gene_counts$Name))
+
+gene_counts <- gene_counts[-which(gene_counts[, 1] %in% rows_to_drop), ]
+
+# Are the expected number of genes remaining?; Yes
+nrow(gene_counts) # 53,325
+nrow(xchr) + nrow(ychr) # 2,875
 
 #_______________________________________________________________________________
 # Sample pre-processing
@@ -29,7 +51,7 @@ gene_counts <- gene_counts[, 3:ncol(gene_counts)]
 
 # Replace . to - in colnames
 colnames(gene_counts) <- str_replace_all(colnames(gene_counts),
-										 pattern = "\\.", "-")
+                                         pattern = "\\.", "-")
 
 # Number of samples in count df
 ncol(gene_counts) # 17,382
@@ -57,11 +79,8 @@ ncol(gene_counts) # 2,146
 identical(colnames(gene_counts), rownames(meta)) # TRUE
 
 #_______________________________________________________________________________
-# Filter genes by expression in each sex and voom normalize 
+# Filter genes by expression in each sex and voom normalize
 #_______________________________________________________________________________
-# Make factor indicating sex of samples for filtering
-#sex <- factor(meta$Sex)
-
 # Make design matrix to use with voom
 design <- model.matrix(~meta$Sex)
 rownames(design) <- colnames(gene_counts)
@@ -71,71 +90,26 @@ nrow(gene_counts) # 56,200
 
 # Remove genes with cpm < 1 in each sex
 keep <- filterByExpr(gene_counts, design = design,
-					 min.count = 1, min.prop = 0.5)
+                     min.count = 1, min.prop = 0.5)
 gene_counts <- gene_counts[keep, ]
 
 # How many genes are left after filtering?
-nrow(gene_counts) # 35,610
+nrow(gene_counts) # 34,341
 
 # limma-voom normalization
-gene_counts <- voom(gene_counts, design=design)
+gene_counts <- voom(gene_counts, design = design)
 
 # Convert gene_counts (EList object) to matrix for use w/ umap
 gene_counts <- data.matrix(gene_counts)
 
 #_______________________________________________________________________________
-# umap 
+# umap
 #_______________________________________________________________________________
-# gene_counts is an expression matrix, with genes as rows and samples (libraries) as columns. 
-# For this particular matrix, we used the partial residuals of a model after removing technical covariates. 
-# If this doesn't apply to your data, use your voom-normalized expression matrix
-# n_neighbors and min_dist are flexible parameters: 
-# see https://umap-learn.readthedocs.io/en/latest/parameters.html for explanation
+# gene_counts is an expression matrix: genes as rows and samples as columns.
 start_t <- Sys.time()
 a <- umap(gene_counts, n_neighbors = 50, min_dist = 0.5)
 end_t <- Sys.time()
-print(paste("Elapsed time:", end_t-start_t))
+print(paste("Elapsed time:", end_t - start_t))
 
 # Write umap object to file
-write.table(a$layout, file="umap.txt", sep=" ")
-
-# extract the projections (a$layout) and join it with metadata (particularly the 'Region' column)
-# full.covariates is a vector of model covariate names (in case you want to plot by those as well)
-#a.umap = data.frame(as.data.frame(a$layout),meta[,c(full.covariates,'Region')])
-#
-## Plot UMAP projections and color by Region.
-## region.colors is a custom vector of pretty colors.
-## batch.variable is a length=1 character vector that contains the name of our batch variable
-#p = ggplot(a.umap,aes_string('V1','V2',color='Region',shape=batch.variable)) +
-#	geom_point(size=1.25) +
-#	scale_color_manual(values=region.colors) +
-#	theme_classic(base_size=18) +
-#	xlab('UMAP 1') +
-#	ylab('UMAP 2') +
-#	coord_fixed() +
-#	theme(axis.ticks=element_blank(),axis.text=element_blank())
-#ggsave(p,file='figures/data_visualization_dimension_reduction_umap_batched.pdf',useDingbats=FALSE)
-#
-## Redo the plot, this time without highlighting batch
-#p = ggplot(a.umap,aes_string('V1','V2',color='Region')) +
-#	geom_point(size=1.25) +
-#	scale_color_manual(values=region.colors) +
-#	theme_classic(base_size=18) +
-#	xlab('UMAP 1') +
-#	ylab('UMAP 2') +
-#	coord_fixed() +
-#	theme(axis.ticks=element_blank(),axis.text=element_blank())
-#ggsave(p,file='figures/data_visualization_dimension_reduction_umap.pdf',useDingbats=FALSE)
-#
-## Redo the plot, this time showing color AND shape to better differentiate points since many colors look similar
-#p = ggplot(a.umap,aes_string('V1','V2',color='Region',shape='Region')) +
-#	geom_point(size=1.25) +
-#	scale_color_manual(values=region.colors) +
-#	scale_shape_manual(values=region.shapes) +
-#	theme_classic(base_size=18) +
-#	xlab('UMAP 1') +
-#	ylab('UMAP 2') +
-#	coord_fixed() +
-#	guides(shape = guide_legend(override.aes = list(size = 3))) +
-#	theme(axis.ticks=element_blank(),axis.text=element_blank())
-#ggsave(p,file='figures/data_visualization_dimension_reduction_umap_shapes.pdf',useDingbats=FALSE)
+write.table(a$layout, file = PROJECTION, sep = " ")
