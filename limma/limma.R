952edd1a8f43039c5f1969aadb8b4d1baebc7d83
Mollie Peters <mjpete11@asu.edu>
2020-08-30 17:38:44 -0700

    added age and sex * age covars

--- limma/limma.R
+++ limma/limma.R
@@ -1,8 +1,8 @@
 #!/usr/bin/env Rscript
 
-#--------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Differential gene expression with limma-voom 
-#--------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
 #Load packages                                                                 
 library(limma)
@@ -11,136 +11,110 @@ library(readr)
 library(stringr)
 
 # Constants
-CWD <- "/scratch/mjpete11/human_monkey_brain/limma/"
-BASE <- "/scratch/mjpete11/human_monkey_brain/metadata"
-METADATA <- file.path(BASE, "metadata.csv")
-COUNTS <- file.path(BASE, "GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.gct")
+BASE <- "/scratch/mjpete11/human_monkey_brain"
+METADATA <- file.path(BASE, "data/metadata/metadata.csv")
+COUNTS <- file.path(BASE, "data/expression_matrices/output/filtered_counts_with_sex_chr.csv")
 
-#--------------------------------------------------------------------------------------------------
-# Data Preprocessing and normalization 
-#--------------------------------------------------------------------------------------------------
-# Read Metadata CSV.                                                            
-Samples <- read.csv(METADATA, header = TRUE, stringsAsFactors=FALSE)
+# Read in files
+meta <- read.csv(METADATA, header = TRUE, stringsAsFactors=FALSE)
+counts <- read.csv(COUNTS, header = TRUE, stringsAsFactors = FALSE)
 
+#------------------------------------------------------------------------------
+# Split counts by region  
+#------------------------------------------------------------------------------
 # Set rownames of metadata object equal to sample names.                        
-rownames(Samples) <- Samples$Sample   
+rownames(meta) <- meta$Sample   
 
 # Set tissues column as factor to add levels attribute
-Samples$Tissue <- as.factor(Samples$Tissue)
-
-# Read in count matrix
-cts <- read.table(COUNTS, sep="\t")
+meta$Tissue <- as.factor(meta$Tissue)
 
 # Replace . to - in colnames
-colnames(cts) <- str_replace_all(colnames(cts),pattern = "\\.","-")
-
-# Identify sample in metadata that does not have count data and drop from metadata
-setdiff(Samples$Sample, colnames(cts)) # "GTEX-13N2G-0011-R2a-SM-5MR4Q"
-Samples <- Samples[!grepl("GTEX-13N2G-0011-R2a-SM-5MR4Q", Samples$Sample),]
-
-# Number of samples with count data after subsetting with metadata
-ncol(cts) == nrow(Samples) # 1,340 samples total
+colnames(counts) <- str_replace_all(colnames(counts),pattern = "\\.","-")
 
 # Metadata split into list of dfs by tissue
-Meta <- list()
-for(i in 1:length(levels(Samples$Tissue))){
-      Meta[[i]] <- Samples[Samples$Tissue == levels(Samples$Tissue)[i],]
+meta_lst <- list()
+for(i in 1:length(levels(meta$Tissue))){
+      meta_lst[[i]] <- meta[meta$Tissue == levels(meta$Tissue)[i],]
 }
-names(Meta) <- levels(Samples$Tissue)
+names(meta_lst) <- levels(meta$Tissue)
 
 # Function to split count matrix into list of dfs
-Tissues <- names(Meta)
+tissues <- names(meta_lst)
 
 Split_Cols <- function(w, z){
-  names <- which(colnames(w) %in% subset(Samples[['Sample']], Samples[['Tissue']] == z))
-  cts_lst <- cts[, names]
-  return(cts_lst)
+  res <- which(colnames(w) %in% subset(meta[['Sample_ID']], meta[['Tissue']] == z))
+  counts_lst <- counts[, res]
+  return(counts_lst)
 }
 
 # Split count matrix into list of dfs
-cts_lst <- list()
-for (i in Tissues){
-      cts_lst[[i]] <- Split_Cols(w=cts, z=i)
+counts_lst <- list()
+for (i in tissues){
+      counts_lst[[i]] <- Split_Cols(w=counts, z=i)
 }
 
-#--------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Sanity check; make sure samples are in same order in count dfs and metadata dfs
-#--------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 Check_Order <- function(x, z){
-    cts_lst <- identical(colnames(x), rownames(z))
-    return(cts_lst)
+    counts_lst <- identical(colnames(x), rownames(z))
+    return(counts_lst)
 }
-all(Map(Check_Order, x=cts_lst, z=Meta)) # TRUE
+all(as.logical(Map(Check_Order, x=counts_lst, z=meta_lst))) # TRUE
 
-#--------------------------------------------------------------------------------------------------
-# Make list of DGEList objects with corresponding design matrix 
-#--------------------------------------------------------------------------------------------------
-# Create design matrix: Step 1
-# Make list of factors
-Groups <- lapply(Meta, function(x) factor(x[['Sex']]))
+#------------------------------------------------------------------------------
+# Make list of DGEList objecounts with corresponding design matrix 
+#------------------------------------------------------------------------------
+# Test: Make DGEList object for one region; design mat, then limma-voom
+sex.groups <- lapply(meta_lst, function(x) factor(x[['Sex']]))
+age.groups <- lapply(meta_lst, function(x) factor(x[['Age']]))
+isc.groups <- lapply(meta_lst, function(x) as.numeric(x[['Ischemic_Time']]))
+rin.groups <- lapply(meta_lst, function(x) as.numeric(x[['RIN']]))
 
-# Combine count dfs and factors to make list of DGEList objects; 
-# required object class for limma
-DGE_Func <- function(d, l){
-    cts_lst <- DGEList(d, group=l)
-    return(cts_lst)
+## Try limma voom on one region
+#tmp.dge <- DGEList(counts_lst[[1]], group = sex.groups[[1]])
+#head(tmp.dge)
+##tmp.mat <- model.matrix(~0 + sex.groups[[1]] + age.groups[[1]] + sex.groups[[1]]:age.groups[[1]] + isc.groups[[1]] + rin.groups[[1]], data = tmp.dge[['samples']])
+## Skipt RIN and ischemic time for now --> leads to 'coefficients not estimable' errors
+#tmp.mat <- model.matrix(~0 + sex.groups[[1]] + age.groups[[1]] + sex.groups[[1]]:age.groups[[1]], data = tmp.dge[['samples']])
+##colnames(mat) <- c("female", "male")
+#head(tmp.mat)
+#tmp.v <- voom(tmp.dge, tmp.mat)
+#head(tmp.v)
+#tmp.fit <- lmFit(tmp.v, tmp.mat)
+#head(tmp.fit)
+#tmp.fit <- eBayes(tmp.fit)
+#head(tmp.fit)
+
+# Function to 1) makde DGEList objects, deign matric, then limma-voom per region
+dge_by_region <- function(dat, sex, age, isc, rin) {
+	dge <- DGEList(dat, group = sex)
+#	design <- model.matrix(~0 + sex + age + sex:age + isc + rin, data = dge[['samples']])
+	design <- model.matrix(~0 + sex + age + sex:age, data = dge[['samples']])
+	v <- voom(dge, design)
+	fit <- lmFit(v, design)
+	fit <- eBayes(fit) # compute t-stat, f-stat, and log-odds of DE
 }
-DGE_Lst <- Map(DGE_Func, d=cts_lst, l=Groups)
 
-# Create design matrix: Step 2; Part 1
-Model_Func <- function(fc, d){
-    x <- model.matrix(~0 + fc, data = d[['samples']])
-    colnames(x) <- gsub("fc", "", colnames(x)) # adds var name to col name; drop it
-    return(x)
+# Apply to each region
+dge_res <- Map(dge_by_region, dat = counts_lst, sex = sex.groups, age = age.groups)
+exists('dge_res')
+
+# Add gene ID column to results
+
+
+# Combine the betas (second col in coeff matrix) with the sqrt standard error (var^2)
+# Write the female coefficient and the square root of the standard error
+#test <- data.frame(beta=dge_res[[1]][['coefficients']][,1], sqrt_SE=sqrt(dge_res[[1]][['s2.post']]))
+
+make_tables <- function(x){
+    counts_lst <- data.frame(beta=x[['coefficients']][,2], sqrt_SE=sqrt(x[['s2.post']]))
+    return(counts_lst)
 }
-Design <- Map(Model_Func, fc=Groups, d=DGE_Lst)
-
-#--------------------------------------------------------------------------------------------------
-# Filter genes by cpm expression level 
-#--------------------------------------------------------------------------------------------------
-# Keep only genes with cpm > 1 in at least half the samples for each tissue type
-# How many cols have cpm > 1 in each row; return TRUE if that value >= half the number of samples 
-Keep <- lapply(DGE_Lst, function(x){rowSums(cpm(x[['counts']]) > 1) >= ceiling(ncol(x[['counts']])/2)})
-
-# Do all genes pass filter?
-all(sapply(Keep, function(x) all(x) == TRUE)) # FALSE
-
-# Range of genes left across conditions
-sapply(Keep, function(x) sum(x)) # 16,486-18,273 
-
-# How many genes are left is I use a more stringent filter: cpm > 1 in all samples 
-Keep.test <- lapply(DGE_Lst, function(x){rowSums(cpm(x[['counts']]) > 1) >= ceiling(ncol(x[['counts']]))})
-
-# Range of genes left across conditions after applying more stringent filter
-sapply(Keep.test, function(x) sum(x)) # 10-12,539 
-
-# Apply filter (cpm >1 in at least half the samples) to list of DGEList object
-Filter_Func <- function(x, k){
-      x <- x[k, , keep.lib.sizes=FALSE]
-}
-DGE_Lst <- Map(Filter_Func, x=DGE_Lst, k=Keep)
-
-# TMM Normalization
-DGE_Lst <- lapply(DGE_Lst, calcNormFactors)
-
-#--------------------------------------------------------------------------------------------------
-# Voom
-#--------------------------------------------------------------------------------------------------
-# Apply voom (rather than plain limma) since library sizes are highly variable
-Voom_Func <- function(x, d){
-    v <- voom(x, d, plot=TRUE)
-    fit <- lmFit(v, d)
-    fit <- eBayes(fit) # compute t-stat, f-stat, and log-odds of DE
-    return(fit)
-}
-Voom_Res <- Map(Voom_Func, x=DGE_Lst, d=Design)
-
-# Combine the betas (second col in coeff matrix) with the standard error (var^2)
-Make_Tables <- function(x){
-    cts_lst <- data.frame(Beta=x[['coefficients']][,2], SE=x[['s2.post']])
-    return(cts_lst)
-}
-Table_Lst <- lapply(Voom_Res, Make_Tables)
+table_lst <- lapply(dge_res, make_tables)
 
 # Write tables 
-sapply(names(Table_Lst), function(x) write.table(Table_Lst[[x]], file=paste0(DIR, x, ".csv"))) 
+sapply(names(table_lst), function(x) write.csv(table_lst[[x]],
+       file = paste0("/scratch/mjpete11/human_monkey_brain/limma/tables/", x, ".csv"), 
+       row.names = FALSE))
+
