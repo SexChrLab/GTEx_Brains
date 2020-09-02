c76f718684eab1bed387ff1e1ddcb64aba9dad70
Mollie Peters <mjpete11@asu.edu>
2020-09-02 16:25:01 -0700

    changed size of random subset

--- mashr/mashr.R
+++ mashr/mashr.R
@@ -25,38 +25,45 @@ limma_lst <- Map(fread, header=TRUE, list.files(path = p1, pattern = "*.csv", fu
 
 # Assign names to dfs in list
 names(limma_lst) <- levels(as.factor(meta$Tissue))
-names(limma_lst)
 
 # Add gene name and id column
-limma_lst <- lapply(limma_lst, function(x) cbind(counts[, 1], x))
+#limma_lst <- lapply(limma_lst, function(x) cbind(counts[, 1], x))
 
 # Combine the first column on every df in list into one matrix 
 # Name columns in each df, then choose which to drop 
 name_drop <- function(x, d){
     x <- data.frame(x)
-    colnames(x) <- c("V1", "beta", "sqrt_SE")
+    colnames(x) <- c("gene_ID", "beta", "st_dev")
     drp <- c(d)
     x <- x[,!colnames(x) %in% drp]
     return(x)
 }    
-effect_lst <- Map(name_drop, x=limma_lst, d="sqrt_SE")
-sqrt_s2_lst <- Map(name_drop, x=limma_lst, d="beta")
+effect_lst <- Map(name_drop, x=limma_lst, d="st_dev")
+st_dev_lst <- Map(name_drop, x=limma_lst, d="beta")
 
 # Combine the betas into one matrix
-effect_df <- Reduce(function(...) merge(..., by = "V1", all = TRUE), effect_lst)
+effect_df <- Reduce(function(...) merge(..., by = "gene_ID", all = TRUE), effect_lst)
 colnames(effect_df) <- c("gene_ID", levels(as.factor(meta$Tissue)))
 effect_mat <- as.matrix(effect_df[, 2:ncol(effect_df)])
 
-# Cobine the sqrt(SEs) into one matrix
-sqrt_s2_df <- Reduce(function(...) merge(..., by = "V1", all = TRUE), sqrt_s2_lst)
-colnames(sqrt_s2_df) <- c("gene_ID", levels(as.factor(meta$Tissue)))
-sqrt_s2_mat <- as.matrix(sqrt_s2_df[, 2:ncol(sqrt_s2_df)])
+# The gene order will be different in the output
+# Check a random gene and make sure the corresponding values are correct
+#lapply(limma <- lst, function(x) x[which(x$V1=="ENSG00000279457.4"),2])
 
-# ________________________________________________________________________________________________________  
+# Cobine the sqrt(SEs) into one matrix
+st_dev_df <- Reduce(function(...) merge(..., by = "gene_ID", all = TRUE), st_dev_lst)
+colnames(st_dev_df) <- c("gene_ID", levels(as.factor(meta$Tissue)))
+st_dev_mat <- as.matrix(st_dev_df[, 2:ncol(st_dev_df)])
+
+# Add rownames to the effect (Bhat) and st dev (Shat) matrices so they are included in the output
+rownames(effect_mat) <- effect_df$gene_ID
+rownames(st_dev_mat) <- effect_df$gene_ID
+
+#________________________________________________________________________________________________________  
 # sanity check 
-# ________________________________________________________________________________________________________  
+#________________________________________________________________________________________________________  
 sanity <- function(e, s){
-    # Do effect and sqrt_s2_matrices have same nrow and rownames in same order?
+    # Do effect and st_dev_matrices have same nrow and rownames in same order?
     print(nrow(s) == nrow(e)) # TRUE
     print(identical(rownames(s), rownames(e))) # TRUE
 
@@ -71,13 +78,13 @@ sanity <- function(e, s){
    
     }
 # Expect: 4T, 2F
-sanity(e=effect_mat, s=sqrt_s2_mat)
+sanity(e=effect_mat, s=st_dev_mat)
 
 # ________________________________________________________________________________________________________  
 # Mashr analysis; NSM lab approach
 # ________________________________________________________________________________________________________  
 # Create the mashr data object
-mash_limma <- mash_set_data(Bhat=effect_mat, Shat=sqrt_s2_mat)
+mash_limma <- mash_set_data(Bhat=effect_mat, Shat=st_dev_mat)
 
 # Apply multivariate adaptive shrinkage method in initial mode ("naive" run)
 # Compute canonical covariance matrix
@@ -85,11 +92,19 @@ U_c <- cov_canonical(mash_limma)
 m_c <- mash(mash_limma, U_c) 
 
 # Save initial results and pairwise sharing matrix
+# New vignette uses get fsr() (false sign rate); investigate later
 strong_limma <- get_significant_results(m_c, thresh=0.05, sig_fn=ashr::get_lfdr)
 shared_limma <- get_pairwise_sharing(m_c, factor=0.05)
 
+# Print IDs of strong genes to standard output
+print(strong_limma)
+
 # Get a randomized set of results with the same length as the significant results
-random_limma <- sample(1:nrow(mash_limma$Bhat), length(strong_limma))
+#random_limma <- sample(1:nrow(mash_limma$Bhat), length(strong_limma))
+
+# Increase size of random subset to 50% of input data because we do not expect
+# most genes to be significant
+random_limma <- sample(1:nrow(mash_limma$Bhat), round(nrow(effect_mat)/2))
 
 # Estimate null correlation on random subset of the data
 # The resulting correlation matrix (vhat_limma) will be used to construct the "random" and "strong"mashr datasets
@@ -115,27 +130,31 @@ U_c_random <- cov_canonical(mash_limma_random)
 # In contrast to the earlier "naive" model, empirical covariance matrices from the extreme deconvolution and canonical methods are used
 m_limma <- mash(mash_limma_random, Ulist=c(U_ed_limma, U_c_random), outputlevel=1)
 
-# Now run mashr on the strong subset, this time substituting the empirical value of g
-m2_limma <- mash(mash_limma_strong, g=get_fitted_g(m_limma), fixg=TRUE)
+# Run mashr on the strong subset, this time substituting the empirical value of g
+#m2_limma <- mash(mash_limma_strong, g=get_fitted_g(m_limma), fixg=TRUE)
+
+# Use covar matrix learned using strong effects, but run on the entire mash data set.
+m2_limma <- mash(mash_limma, g=get_fitted_g(m_limma), fixg=TRUE)
 
 # Calculate pairwise sharing
 limma_share <- get_pairwise_sharing(m2_limma, factor=0.5)
+
+# Posterior local false sign rate
+lfsr_limma <- get_lfsr(m2_limma)
 
 # Set a flase sign rate cutoff
 fsr_cutoff <- 0.2
 
 # Get number of significant genes in each region
-sig_genes <- apply(get_lfsr(m2_limma), 2, function(x) sum(x < fsr_cutoff))
+sig_genes <- apply(lfsr_limma, 2, function(x) sum(x < fsr_cutoff))
 sig_genes <- data.frame(region = names(sig_genes), num_sig_genes = sig_genes) 
 
 # Get posterior values
 # Posterior betas
 beta_limma <- as.data.frame(get_pm(m2_limma))
 
-# Posterior local false sign rate
-lfsr_limma <- get_lfsr(m2_limma)
-
 # Write to file
+saveRDS(m2_limma, file = "/scratch/mjpete11/human_monkey_brain/mashr/output/mashr_results.rds")
 write.csv(limma_share, file = "/scratch/mjpete11/human_monkey_brain/mashr/output/limma_share.csv")
 write.csv(sig_genes, file = "/scratch/mjpete11/human_monkey_brain/mashr/output/sig_genes.csv", row.names = FALSE)
 write.csv(beta_limma, file = "/scratch/mjpete11/human_monkey_brain/mashr/output/beta_limma.csv")
